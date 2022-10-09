3adaf1ca7e5b663bb54487a857b6107ac00c4bfd
Chris Davis <contact@nthstate.com>
2022-10-09 12:41:31 +0100

    add log

--- run.zsh
+++ run.zsh
@@ -3,6 +3,6 @@
 SRC="${BUILD_DIR%Build/*}/SourcePackages/checkouts/ProductAnalytics"
 cd $SRC
 swift build
-echo pwd
+echo "Build Complete"
 echo "Try to run: .build/x86_64-apple-macosx/${CONFIGURATION:lower}/ProductAnalytics"
-.build/x86_64-apple-macosx/${CONFIGURATION:lower}/ProductAnalytics
+.build/x86_64-apple-macosx/debug/ProductAnalytics
